6ef6b98a38b6140d15ff69f97d63b07eaa8bf2a5
HEKKOD <musa28.mirac28@gmail.com>
2026-01-12 19:57:47 +0300

    Add files via upload

---
+++ Hesap makinesi/Hesap.py
@@ -0,0 +1,487 @@
+print("""
+##################################################
+#                                                #
+#                  HESAP MAKİNESİ                #
+#                                                #
+##################################################
+""")
+print("1) + Toplama")
+print("2) - Çıkarma")
+print("3) / yada ÷ Bölme")
+print("4) * Çarpama")
+print("5) % Mod alma")
+print("")
+print("1) 2 sayı ile")
+print("2) 3 sayı ile")
+print("3) 4 sayı ile")
+print("4) 5 sayı ile")
+print("5) 6 sayı ile")
+print("6) 7 sayı ile")
+print("7) 8 sayı ile")
+print("8) 9 sayı ile")
+print("9) 10 sayı ile")
+while True:
+   islem_sembolu = int(input("Lütfen hangi işlemi yapcaksanız onu seciniz -->"))
+   islem_sayisi = int(input("Lütfen kaç sayı ile işlem yapıcaksınız onu seciniz -->"))
+
+#işlem sayısı 1
+   if islem_sayisi == 1 and islem_sembolu == 1:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     toplam1 = a + b
+     print(toplam1)
+
+   if islem_sayisi == 1 and islem_sembolu == 2:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     toplam2 = a - b
+     print(toplam2)
+   
+   if islem_sayisi == 1 and islem_sembolu == 3:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     toplam3 = a / b
+     print(toplam3)
+     
+   if islem_sayisi == 1 and islem_sembolu == 4:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     toplam4 = a * b
+     print(toplam4)
+     
+   if islem_sayisi == 1 and islem_sembolu == 5:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     toplam5 = a % b
+     print(toplam5)
+     
+#işlem sayısı 2
+   if islem_sayisi == 2 and islem_sembolu == 1:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     toplam6 = a + b + c
+     print(toplam6)
+     
+   if islem_sayisi == 2 and islem_sembolu == 2:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     toplam7 = a - b - c
+     print(toplam7)
+
+   if islem_sayisi == 2 and islem_sembolu == 3:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     toplam8 = a / b / c
+     print(toplam8)
+
+   if islem_sayisi == 2 and islem_sembolu == 4:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     toplam9 = a * b * c
+     print(toplam9)
+
+   if islem_sayisi == 2 and islem_sembolu == 5:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     toplam10 = a % b % c
+     print(toplam10)
+
+#İşlem sayısı 3
+
+   if islem_sayisi == 3 and islem_sembolu == 1:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     toplam11 = a + b + c + d
+     print(toplam11)
+
+   if islem_sayisi == 3 and islem_sembolu == 2:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     toplam12 = a - b - c - d
+     print(toplam12)
+
+   if islem_sayisi == 3 and islem_sembolu == 3:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     toplam13 = a / b / c / d
+     print(toplam13)
+
+   if islem_sayisi == 3 and islem_sembolu == 4:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     toplam14 = a * b * c * d
+     print(toplam14)
+
+   if islem_sayisi == 3 and islem_sembolu == 5:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     toplam15 = a % b % c % d
+     print(toplam15)
+
+#İşlem sayısı 4
+   if islem_sayisi == 4 and islem_sembolu == 1:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     toplam16 = a + b + c + d + e
+     print(toplam16)
+     
+   if islem_sayisi == 4 and islem_sembolu == 2:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     toplam17 = a - b - c - d - e
+     print(toplam17)
+
+   if islem_sayisi == 4 and islem_sembolu == 3:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     toplam18 = a / b / c / d / e
+     print(toplam18)
+     
+   if islem_sayisi == 4 and islem_sembolu == 4:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     toplam19 = a * b * c * d * e
+     print(toplam19)
+     
+   if islem_sayisi == 4 and islem_sembolu == 5:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     toplam20 = a % b % c % d % e
+     print(toplam20)
+
+#İşlem sayısı 5
+   if islem_sayisi == 5 and islem_sembolu == 1:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     toplam21 = a + b + c + d + e + f
+     print(toplam21)
+
+   if islem_sayisi == 5 and islem_sembolu == 2:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     toplam22 = a - b - c - d - e - f
+     print(toplam22)
+
+   if islem_sayisi == 5 and islem_sembolu == 3:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     toplam23 = a / b / c / d / e / f
+     print(toplam23)
+
+   if islem_sayisi == 5 and islem_sembolu == 4:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     toplam24 = a * b * c * d * e * f
+     print(toplam24)
+
+   if islem_sayisi == 5 and islem_sembolu == 5:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     toplam25 = a % b % c % d % e % f
+     print(toplam25)     
+
+#işlem sayısı 6
+   if islem_sayisi == 6 and islem_sembolu == 1:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     toplam26 = a + b + c + d + e + f + g
+     print(toplam26)
+
+   if islem_sayisi == 6 and islem_sembolu == 2:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     toplam27= a - b - c - d - e - f - g
+     print(toplam27)
+
+   if islem_sayisi == 6 and islem_sembolu == 3:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     toplam28 = a / b / c / d / e / f / g
+     print(toplam28)
+     
+   if islem_sayisi == 6 and islem_sembolu == 4:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     toplam29 = a * b * c * d * e * f * g
+     print(toplam29)
+
+   if islem_sayisi == 6 and islem_sembolu == 5:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     toplam30 = a % b % c % d % e % f % g
+     print(toplam30)
+
+#İşlem sayısı 7
+   if islem_sayisi == 7 and islem_sembolu == 1:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     toplam31 = a + b + c + d + e + f + g + h
+     print(toplam31)
+
+   if islem_sayisi == 7 and islem_sembolu == 2:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     toplam32 = a - b - c - d - e - f - g - h
+     print(toplam32)
+
+   if islem_sayisi == 7 and islem_sembolu == 3:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     toplam33 = a / b / c / d / e / f / g / h
+     print(toplam33)
+
+   if islem_sayisi == 7 and islem_sembolu == 4:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     toplam34 = a * b * c * d * e * f * g * h
+     print(toplam34)
+
+   if islem_sayisi == 7 and islem_sembolu == 5:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     toplam35 = a % b % c % d % e % f % g % h
+     print(toplam35)
+
+#İşlem sayısı 8
+   if islem_sayisi == 8 and islem_sembolu == 1:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     i = int(input("Lütfen sayı giriniz -->"))
+     toplam36 = a + b + c + d + e + f + g + h + i
+     print(toplam36)
+
+   if islem_sayisi == 8 and islem_sembolu == 2:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     i = int(input("Lütfen sayı giriniz -->"))
+     toplam37 = a - b - c - d - e - f - g - h - i
+     print(toplam37)
+
+   if islem_sayisi == 8 and islem_sembolu == 3:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     i = int(input("Lütfen sayı giriniz -->"))
+     toplam38 = a / b / c / d / e / f / g / h / i
+     print(toplam38)
+
+   if islem_sayisi == 8 and islem_sembolu == 4:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     i = int(input("Lütfen sayı giriniz -->"))
+     toplam39 = a * b * c * d * e * f * g * h * i
+     print(toplam39)
+
+   if islem_sayisi == 8 and islem_sembolu == 5:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     i = int(input("Lütfen sayı giriniz -->"))
+     toplam40 = a % b % c % d % e % f % g % h % i
+     print(toplam40)
+
+#İşlem sayısı 9
+   if islem_sayisi == 9 and islem_sembolu == 1:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     i = int(input("Lütfen sayı giriniz -->"))
+     j = int(input("Lütfen sayı giriniz -->"))
+     toplam41 = a + b + c + d + e + f + g + h + i + j
+     print(toplam41)
+
+   if islem_sayisi == 9 and islem_sembolu == 2:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     i = int(input("Lütfen sayı giriniz -->"))
+     j = int(input("Lütfen sayı giriniz -->"))
+     toplam42 = a - b - c - d - e - f - g - h - i - j
+     print(toplam42)
+
+   if islem_sayisi == 9 and islem_sembolu == 3:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     i = int(input("Lütfen sayı giriniz -->"))
+     j = int(input("Lütfen sayı giriniz -->"))
+     toplam43 = a / b / c / d / e / f / g / h / i / j
+     print(toplam43)
+
+   if islem_sayisi == 9 and islem_sembolu == 4:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     i = int(input("Lütfen sayı giriniz -->"))
+     j = int(input("Lütfen sayı giriniz -->"))
+     toplam44 = a * b * c * d * e * f * g * h * i * j
+     print(toplam44)
+
+   if islem_sayisi == 9 and islem_sembolu == 5:
+     a = int(input("Lütfen sayı giriniz -->"))
+     b = int(input("Lütfen sayı giriniz -->"))
+     c = int(input("Lütfen sayı giriniz -->"))
+     d = int(input("Lütfen sayı giriniz -->"))
+     e = int(input("Lütfen sayı giriniz -->"))
+     f = int(input("Lütfen sayı giriniz -->"))
+     g = int(input("Lütfen sayı giriniz -->"))
+     h = int(input("Lütfen sayı giriniz -->"))
+     i = int(input("Lütfen sayı giriniz -->"))
+     j = int(input("Lütfen sayı giriniz -->"))
+     toplam45 = a % b % c % d % e % f % g % h % i % j
+     print(toplam45)
+
+
